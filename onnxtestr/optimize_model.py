52325afdfa92c64322436cc9f03526d25cd46b3b
Melns21 <161919226+Melns21@users.noreply.github.com>
2024-09-19 13:39:17 +0300

    Delete onnxtestr/optimize_model.py

--- onnxtestr/optimize_model.py
+++
@@ -1,8 +0,0 @@
-import onnx 
-from onnxruntime.tools import optimize 
-input_model_path = "E:/t4/onnxtestr/onnxtestr/yolov10n.onnx" 
-output_model_path = "E:/t4/onnxtestr/onnxtestr/yolov10n_optimized.onnx" 
-model = onnx.load(input_model_path) 
-optimized_model = optimize.optimize_model(model) 
-onnx.save_model(optimized_model, output_model_path) 
-print(f"Optimized model saved to {output_model_path}") 
